68cabaaddddc6274f5922f0cd6f0658659441d2f
lee-t <tyrone_lee@hms.harvard.edu>
2022-04-13 18:20:26 -0400

    added dummy stuff

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+NAME = 'myproject'
+VERSION = '1.0'
+AUTHORS = ' Tyrone Lee'
+
+setup(name=NAME,
+      description='Dummy package for this tutorial',
+      version=VERSION,
+      author=AUTHORS,
+      packages=find_packages("."))
